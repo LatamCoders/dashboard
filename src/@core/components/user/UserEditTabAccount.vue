3ecc71d1a10811cbaac55fae71087f0cfa591d34
fernandoMorillo <fernando.morillo0315@gmail.com>
2022-06-07 17:02:03 -0500

    cambio de texto edit y save en perfil CA

--- src/@core/components/user/UserEditTabAccount.vue
+++ src/@core/components/user/UserEditTabAccount.vue
@@ -25,7 +25,7 @@
             :variant="inhabilitar === false ? 'primary' : 'outline-secondary'"
             @click="savedInfo === true ? inhabilitar = true : inhabilitar = true"
         >
-          <span class="d-none d-sm-inline">Update</span>
+          <span class="d-none d-sm-inline">Edit</span>
           <feather-icon
               icon="EditIcon"
               class="d-inline d-sm-none"
@@ -37,7 +37,7 @@
             class="ml-1"
             @click="savedInfo = true"
         >
-          <span class="d-none d-sm-inline">Saved</span>
+          <span class="d-none d-sm-inline">Save</span>
           <feather-icon
               icon="TrashIcon"
               class="d-inline d-sm-none"
